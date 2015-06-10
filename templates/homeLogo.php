83f64a95b3574ef80b559bc51154a74969d9d2b8
tostyle <unplugged2d@gmail.com>
2015-06-10 14:17:27 +0700

    fix home logo page

--- templates/homeLogo.php
+++ templates/homeLogo.php
@@ -1,17 +1,19 @@
 <div class="home-icon-div">
 	
-	<div class="home-icon ">
-		<img src="photo/icon-saving-money.png">
+	<div class="home-icon " id="logo-money">
+		<div class="logo-pic pic-money"></div>
 		<h3>Saving Money</h3>		
 		
 	</div>
-	<div class="home-icon">
-		<img src="photo/icon-saving-power.png">
+	<div class="home-icon" id="logo-power">
+		<div class="logo-pic pic-power"></div>
+	
 		<h3>Saving Power</h3>		
 		
 	</div>
-	<div class="home-icon last">
-		<img src="photo/icon-awards.png">
+	<div class="home-icon last" id="logo-award">
+		<div class="logo-pic pic-award"></div>
+		
 		<h3>Awards</h3>		
 	
 	</div>
