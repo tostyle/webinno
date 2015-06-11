dbc6f6593d108d01e9c4eab4e2be35ddc0cea065
tostyle <unplugged2d@gmail.com>
2015-06-11 16:04:54 +0700

    add map file and fix carrer link

--- templates/career.php
+++ templates/career.php
@@ -4,12 +4,12 @@
 	<h3>INNOVATION TECHNOLOGY</h3>
 	<p>"ร่วมเป็นส่วนหนึ่งในทีมคุณภาพและเติบโตไปพร้อมๆกับพวกเรา"</p>
 	<p class="open-position">ตำแหน่งที่เปิดรับ (คลิกเพื่อกรอกใบสมัคร)</p>
-	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers">ช่างเทคนิค</a></p>
+	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers01">ช่างเทคนิค</a></p>
 	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers02">วิศวกร</a></p>
 	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers03">หัวหน้าช่างเทคนิค</a></p>
-	<p>ผู้จัดการโครงการ</p>
-	<p>เจ้าหน้าที่ธุรการ</p>
-	<p>ธุรการ (ประจำโครงการ)</p>
+	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers04">ผู้จัดการโครงการ</a></p>
+	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers05">เจ้าหน้าที่ธุรการ</a></p>
+	<p><a class="career-link" data-toggle="modal" data-target="#careerModal" href="" id="pic-innovation-careers06">ธุรการ (ประจำโครงการ)</a></p>
 
 	
 </div><!--
