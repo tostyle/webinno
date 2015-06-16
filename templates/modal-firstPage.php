a6451b6647a63aafad44a8d58cd3e0df98135ecb
tostyle <unplugged2d@gmail.com>
2015-06-16 15:56:13 +0700

    modal firstpage no border

--- templates/modal-firstPage.php
+++ templates/modal-firstPage.php
@@ -3,17 +3,12 @@
 
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-body">
-
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-      <div class="modal-body">
          <div class="firstpage-picture">
             <a href="#" id="facebook-popup-link"><img src="photo/index/button-fabook.png" alt=""></a>
          </div>
      
 
-      </div>
+     
     </div>
 
   </div>
