d83132de81f5b05b5282b1358f34e1ec7c0cf7f9
tostyle <unplugged2d@gmail.com>
2015-06-16 16:50:45 +0700

    modal no border in modal service

--- templates/modal-service.php
+++ templates/modal-service.php
@@ -3,11 +3,11 @@
 
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-body">
+     <!--  <div class="modal-body">
 
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-      <div class="modal-body">
+      </div> -->
+      <div >
             <img src="" id="service-detail-picture" alt="">
          <!--  <button type="button" class="close" data-dismiss="modal">&times;</button> -->
       </div>
