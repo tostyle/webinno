88aeafdda90692eaa33c42b1dba76e987069d513
tostyle <unplugged2d@gmail.com>
2015-06-17 15:05:19 +0700

    popup index not center when open pc then fix it

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
       
         // Dividing by two centers the modal exactly, but dividing by three 
         // or four works better for larger screens.
-        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()/2) / 2));
+        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
     }
     // Reposition when a modal is shown
     $('.modal').on('show.bs.modal', reposition);
