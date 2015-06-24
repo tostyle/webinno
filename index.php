250eea10902182565662041376974b3c9d131012
tostyle <unplugged2d@gmail.com>
2015-06-24 10:44:32 +0700

    track library files

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
     <!-- Custom CSS -->
  <!--    <link href="css/full-width-pics.css" rel="stylesheet"> -->
-    <link href="css/mainpage.css" rel="stylesheet">
+    <link href="css/mainpage.php" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
