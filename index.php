06dc508293a62ec916983e4142646b1fe7d30444
tostyle <unplugged2d@gmail.com>
2015-06-29 11:08:05 +0700

    add meta tag

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="innovation technology">
+    <meta name="keywords" content="innovation technology,innovationtechnology,inno">
+    <meta name="author" content="innovation technology">
 
     <title>Innovation Technology</title>
 
