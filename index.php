fea9f54146736f6fc06722bd7c44bf9133293129
tostyle <unplugged2d@gmail.com>
2015-06-30 08:20:42 +0700

    add meta tag

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <meta name="description" content="innovation technology">
-    <meta name="keywords" content="innovation technology,innovationtechnology,inno">
+    <meta name="description" content="innovation technology for enegy solution">
     <meta name="author" content="innovation technology">
+    <meta name="keywords" content="innovation technology,innotechnology,inno,innotech">
 
     <title>Innovation Technology</title>
 
@@ -22,7 +22,7 @@
 
     <!-- Custom CSS -->
  <!--    <link href="css/full-width-pics.css" rel="stylesheet"> -->
-    <link href="css/mainpage.php" rel="stylesheet">
+    <link href="css/mainpage.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
