3d51092e9b703f4bbddfb933cb357af862915aaa
tostyle <unplugged2d@gmail.com>
2015-07-23 15:20:16 +0700

    change to thai sliederf pic

--- templates/homeSlider.php
+++ templates/homeSlider.php
@@ -1,7 +1,7 @@
 <ul class="bxslider">
   <li class="full-pic"><img src="photo/slide-pic/pic-banner-01-en.jpg" /></li>
-  <li class="full-pic"><img src="photo/slide-pic/pic-banner-02-en.jpg" /></li>
-  <li class="full-pic"><img src="photo/slide-pic/pic-banner-03-en.jpg" /></li>
-  <li class="full-pic"><img src="photo/slide-pic/pic-banner-04-en.jpg" /></li>
-  <li class="full-pic"><img src="photo/slide-pic/pic-banner-05-en.jpg" /></li>
+  <li class="full-pic"><img src="photo/slide-pic/pic-banner-02-th.jpg" /></li>
+  <li class="full-pic"><img src="photo/slide-pic/pic-banner-03-th.jpg" /></li>
+  <li class="full-pic"><img src="photo/slide-pic/pic-banner-04-th.jpg" /></li>
+  <li class="full-pic"><img src="photo/slide-pic/pic-banner-05-th.jpg" /></li>
 </ul>
