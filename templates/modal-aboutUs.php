d7c04ec546e75da62523b7331a3ac3e6f8c0a7f9
tostyle <unplugged2d@gmail.com>
2015-07-23 15:06:21 +0700

    fix aboutuspic

--- templates/modal-aboutUs.php
+++ templates/modal-aboutUs.php
@@ -2,8 +2,9 @@
   <div class="modal-dialog modal-lg modal-background modal-aboutUs">
     <!-- Modal content-->
     <div class="modal-content">
+    <a class="boxclose box-aboutUs" id="boxclose" data-dismiss="modal"></a>
          <div class="aboutUs-picture">
-         <a class="boxclose" id="boxclose" data-dismiss="modal"></a>
+        <!--   -->
          	
          		<img data-toggle="modal" href="#aboutUsDetailModal" class="team-detail" id="pic-01" src="photo/our-team/pic-01.jpg" alt="">
          
