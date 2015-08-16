839a2a36820d5cefe110b0e2fdc5cae7070c41fa
tostyle <unplugged2d@gmail.com>
2015-08-16 12:12:33 +0700

    add dashboarc ctrl

--- admin/dashboardCtrl.js
+++ admin/dashboardCtrl.js
@@ -1,7 +1,7 @@
-adminApp.controller('DashboardCtrl', ['$scope','Menu', function ($scope, Menu) {
-
-	var promise = Menu.getMenuList();
-	promise.then(function(menus) {
-    	$scope.menus  = menus;
-	});
+adminApp.controller('DashboardCtrl', ['$scope','$rootScope','Menu', function ($scope,$rootScope, Menu) {
+	// $scope.menus =$rootScope.menus;
+	// var promise = Menu.getMenuList();
+	// promise.then(function(menus) {
+ //    	$scope.menus  = menus;
+	// });
 }])
